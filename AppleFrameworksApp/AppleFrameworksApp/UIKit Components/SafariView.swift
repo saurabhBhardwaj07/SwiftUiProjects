d25d97b13a84fe470f449ac258fd55f8b9aa20e1
Saurabh bhardwaj <srvkr0730@gmail.com>
2024-02-24 23:48:47 +0530

    update uikit controller

--- AppleFrameworksApp/AppleFrameworksApp/UIKit Components/SafariView.swift
+++ AppleFrameworksApp/AppleFrameworksApp/UIKit Components/SafariView.swift
@@ -14,6 +14,8 @@ struct SafariView: UIViewControllerRepresentable{
     func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController  {
         SFSafariViewController(url: url)
     }
+
+    
     
     func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
         
